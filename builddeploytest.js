104f0e9f1deec4ce7106cd2a33a571749a031e19
Carsten Munk <carsten.munk@7bunited.com>
2017-01-25 14:40:08 +0100

    Make authed methods actually work
    Signed-off-by: Carsten Munk <carsten.munk@7bunited.com>

--- builddeploytest.js
+++ builddeploytest.js
@@ -10,13 +10,17 @@ var output = solc.compile({sources: input}, 1);
 
 process.removeAllListeners("uncaughtException");
 
+console.log(output.contracts)
+
+
+/* 
 var logger = {
   log: function(message) {
 //     console.log(message);
   }
-};
+}; */
 
-var port = 8546;
+var port = 8545;
 
 function testToken(account, account2, token)
 {
@@ -28,7 +32,7 @@ function testToken(account, account2, token)
           console.log(event);
     });
     
-    token.issue(10000, { from: account }, function(err, result) { 
+    token.issue(5000*100000, { from: account }, function(err, result) { 
         if (err) { console.log(err); } 
         if (result) {
            token.totalSupply(function(err, result) {
@@ -36,45 +40,44 @@ function testToken(account, account2, token)
            });
            token.balanceOf(account, function(err, result) { 
              console.log("Balance after issue is " + result);
-             token.redeem(5000, { from: account }, function(err, result) {
-                token.totalSupply(function(err, result) {
-                   console.log("Total supply after issue " + result);
-                });
-
-                token.balanceOf(account, function(err, result) { 
-                  console.log("Balance after issue redeem " + result);
-                  
-                  token.transfer(account2, 500, { from: account }, function(err, result) { 
-                       token.balanceOf(account, function(err, result) {
-                           console.log("Balance account1 after transfer " + result);
-                           token.balanceOf(account2, function(err, result) {
-                               console.log("Balance account2 after transfer " + result);
-                           });
-                       });
+             token.transfer(account2, 500, { from: account }, function(err, result) { 
+                   token.balanceOf(account, function(err, result) {
+                      console.log("Balance account1 after transfer " + result);
+                      token.balanceOf(account2, function(err, result) {
+                         console.log("Balance account2 after transfer " + result);
+                      });
+                   });
                   });
                 });
-             });
-           });
         } 
     });
 }
 
-server = TestRPC.server({ logger: logger /*, debug: true, verbose: true */ });
+/* server = TestRPC.server({ logger: logger, debug: true, verbose: true });
 server.listen(port, function(err, state) {
      if (err)
      {
         console.log(err);
         return;
      } 
-
+    */ 
      web3.setProvider(new Web3.providers.HttpProvider("http://localhost:" + port));
 
      web3.eth.getAccounts(function(err, result) {
         var account = result[0];
         var account2 = result[1];    
   
+        console.log(output.contracts["StandardZipperTokenFunctionality"].bytecode);
+
+/*         var PZipToken = web3.eth.contract(JSON.parse(output.contracts["PZipToken"].interface));
+        var pziptoken = PZipToken.at("0xa4d63c03e817fb02da46bb893e1d3e2ecbf4e80b");
+        
+        pziptoken.transfer("0xf726794adf5de38246974a08fbb8933be5d1c705", 500, { from: account, gas: 800000 }, function(err, result) { console.log(result); } );
+        
+        return;
+ */
         var StandardZipperTokenFunctionality = web3.eth.contract(JSON.parse(output.contracts["StandardZipperTokenFunctionality"].interface));
-        var standardZTF = StandardZipperTokenFunctionality.new({ from: account, gas: 1000000, data: "0x" + output.contracts["StandardZipperTokenFunctionality"].bytecode },
+        var standardZTF = StandardZipperTokenFunctionality.new({ from: account, gas: 2000000, data: "0x" + output.contracts["StandardZipperTokenFunctionality"].bytecode },
           function(err, result) {
              if (err) { console.log(err); return; }
              if (standardZTF.address)
@@ -82,7 +85,7 @@ server.listen(port, function(err, state) {
                  console.log("StandardZTF at " + standardZTF.address);
                  
                  var ITokenActionValidator = web3.eth.contract(JSON.parse(output.contracts["ITokenActionValidator"].interface));
-                 var itokenactionvalidator = ITokenActionValidator.new({ from: account, gas: 1000000, data: "0x" + 
+                 var itokenactionvalidator = ITokenActionValidator.new({ from: account, gas: 2000000, data: "0x" + 
                     output.contracts["ITokenActionValidator"].bytecode }, function(err, result) {
                            if (err) { console.log(err); return; }
                            if (itokenactionvalidator.address)
@@ -94,13 +97,13 @@ server.listen(port, function(err, state) {
                  
                                var PZipToken = web3.eth.contract(JSON.parse(output.contracts["PZipToken"].interface));
                                var pziptoken = PZipToken.new(account, itokenactionvalidator.address, "0xf8260e2729e5f618005dc011a36d699bd2e53055",
-                                   { from: account, gas: 1000000, data: "0x" + pzipbytecode }, function(err, result) {
+                                   { from: account, gas: 2000000, data: "0x" + pzipbytecode }, function(err, result) {
                                         if (err) { console.log(err); return; }
                                         if (pziptoken.address)
                                         {
                                            console.log("PZipToken at " + pziptoken.address);
                                            
-                                           testToken(account, account2, pziptoken);
+                                           testToken(account, "0xf726794adf5de38246974a08fbb8933be5d1c705", pziptoken);
                                         }
                                });
                            }
@@ -108,5 +111,5 @@ server.listen(port, function(err, state) {
              }
           });
       });
-});
+/* }); */
 
--- zippertoken.sol
+++ zippertoken.sol
@@ -1,12 +1,16 @@
 pragma solidity ^0.4.4;
 
 /* Based on zeppelin-solidity */
+contract IAuthedForwarder
+{
+    function whitelist(bytes4 sig);
+}
 
 /*
  * ERC20 interface
  * see https://github.com/ethereum/EIPs/issues/20
  */
-contract ERC20 { /* interface */ 
+contract ERC20 { /* interface */
   function totalSupply() constant returns (uint);
   function balanceOf(address who) constant returns (uint);
   function allowance(address owner, address spender) constant returns (uint);
@@ -14,7 +18,7 @@ contract ERC20 { /* interface */
   function transfer(address to, uint value) returns (bool ok);
   function transferFrom(address from, address to, uint value) returns (bool ok);
   function approve(address spender, uint value) returns (bool ok);
-  
+
   event Transfer(address indexed from, address indexed to, uint value);
   event Approval(address indexed owner, address indexed spender, uint value);
 }
@@ -66,29 +70,29 @@ contract ITokenActionValidator
     {
         return (true, 0, this);
     }
-    
+
     function transferFromOK(address _caller, address _from, address _to, uint _value) returns  (bool success, uint fee, address feeReceiver)
     {
         return (true, 0, this);
     }
-    
+
     function approveOK(address _caller, address _spender, uint _value) returns (bool success)
     {
         return true;
     }
-    
+
     function getAuthedForwarder() constant returns (address forwarder)
     {
         return this;
     }
-    
-    function setAuthDataOK(address _caller, ITokenActionValidator _validator,  address _authedForwarder) returns (bool success)
+
+    function setAuthDataOK(address _caller, ITokenActionValidator _validator, IAuthedForwarder _authedForwarder) returns (bool success)
     {
         return true;
     }
 }
 
-/** 
+/**
  * Basic functionality of the token is divided out into a library to limit token size
  */
 library StandardZipperTokenFunctionality
@@ -98,24 +102,24 @@ library StandardZipperTokenFunctionality
       mapping (address => mapping (address => uint)) allowed;
       uint totalSupply;
       ITokenActionValidator validator;
-      address authedForwarder;
+      IAuthedForwarder authedForwarder;
   }
-  
+
   event Transfer(address indexed from, address indexed to, uint value);
   event Approval(address indexed owner, address indexed spender, uint value);
-  
+
   function transfer(data storage self, address _caller, address _to, uint _value) returns (bool success) {
     var (tOK, fee, feeReceiver) = self.validator.transferOK(_caller, _to, _value);
-    
+
     if (!tOK)
         return false;
-     
+
     var valueplusfee = SafeMath.safeAdd(_value, fee);
-    
+
     self.balances[_caller] = SafeMath.safeSub(self.balances[_caller], valueplusfee);
     self.balances[_to] = SafeMath.safeAdd(self.balances[_to], _value);
     Transfer(msg.sender, _to, _value);
-    
+
     if (fee > 0)
     {
         self.balances[feeReceiver] = SafeMath.safeAdd(self.balances[feeReceiver], fee);
@@ -134,8 +138,8 @@ library StandardZipperTokenFunctionality
 
     // This is technically done in safeSub below but it's prettier to not throw.
     if (_value >= _allowance)
-        return false; 
-        
+        return false;
+
     self.balances[_to] = SafeMath.safeAdd(self.balances[_to], _value);
     self.balances[_from] = SafeMath.safeSub(self.balances[_from], _value);
     self.allowed[_from][_caller] = SafeMath.safeSub(_allowance, _value);
@@ -150,31 +154,37 @@ library StandardZipperTokenFunctionality
     Approval(_caller, _spender, _value);
     return true;
   }
-  
-  function setAuthData(data storage self, ITokenActionValidator _newvalidator, address _authedForwarder) returns (bool success)
+
+  function setAuthData(data storage self, ITokenActionValidator _newvalidator, IAuthedForwarder _authedForwarder) returns (bool success)
   {
     if (!self.validator.setAuthDataOK(msg.sender, _newvalidator, _authedForwarder))
         return false;
     self.validator = _newvalidator;
     self.authedForwarder = _authedForwarder;
+    self.authedForwarder.whitelist(bytes4(bytes32(sha3("transferAuthed(address,address,uint256)"))));
+    self.authedForwarder.whitelist(bytes4(bytes32(sha3("transferFromAuthed(address,address,address,uint256)"))));
+    self.authedForwarder.whitelist(bytes4(bytes32(sha3("approveAuthed(address,address,uint256)"))));
     return true;
   }
 }
 
 contract StandardZipperToken is Zipper_ERC20 {
   StandardZipperTokenFunctionality.data internal data;
-  
-  function StandardZipperToken(ITokenActionValidator _validator, address _authedForwarder)
+
+  function StandardZipperToken(ITokenActionValidator _validator, IAuthedForwarder _authedForwarder)
   {
     data.validator = _validator;
     data.authedForwarder = _authedForwarder;
+    data.authedForwarder.whitelist(bytes4(bytes32(sha3("transferAuthed(address,address,uint256)"))));
+    data.authedForwarder.whitelist(bytes4(bytes32(sha3("transferFromAuthed(address,address,address,uint256)"))));
+    data.authedForwarder.whitelist(bytes4(bytes32(sha3("approveAuthed(address,address,uint256)"))));
   }
-  
+
   function totalSupply() constant returns (uint)
   {
     return data.totalSupply;
   }
-  
+
   function balanceOf(address _who) constant returns (uint)
   {
     return data.balances[_who];
@@ -193,7 +203,7 @@ contract StandardZipperToken is Zipper_ERC20 {
   {
     return StandardZipperTokenFunctionality.transferFrom(data, msg.sender, from, to, value);
   }
-  
+
   function approve(address spender, uint value) returns (bool ok)
   {
     return StandardZipperTokenFunctionality.approve(data, msg.sender, spender, value);
@@ -201,30 +211,30 @@ contract StandardZipperToken is Zipper_ERC20 {
 
   function transferAuthed(address caller, address to, uint value) returns (bool ok)
   {
-    if (msg.sender != data.authedForwarder)
+    if (msg.sender != address(data.authedForwarder))
       return false;
-      
-    return StandardZipperTokenFunctionality.transfer(data, caller, to, value);   
+
+    return StandardZipperTokenFunctionality.transfer(data, caller, to, value);
   }
 
   function transferFromAuthed(address caller, address from, address to, uint value) returns (bool ok)
   {
-    if (msg.sender != data.authedForwarder)
+    if (msg.sender != address(data.authedForwarder))
        return false;
-       
+
     return StandardZipperTokenFunctionality.transferFrom(data, caller, from, to, value);
   }
-  
+
   function approveAuthed(address caller, address spender, uint value) returns (bool ok)
   {
-    if (msg.sender != data.authedForwarder)
+    if (msg.sender != address(data.authedForwarder))
         return false;
-        
+
     return StandardZipperTokenFunctionality.approve(data, caller, spender, value);
   }
-  
+
   /* maintainence */
-  function setAuthData(ITokenActionValidator _newvalidator, address _authedForwarder) returns (bool success)
+  function setAuthData(ITokenActionValidator _newvalidator, IAuthedForwarder _authedForwarder) returns (bool success)
   {
     return StandardZipperTokenFunctionality.setAuthData(data, _newvalidator, _authedForwarder);
   }
@@ -237,13 +247,13 @@ contract StandardZipperToken is Zipper_ERC20 {
 contract SimpleIssuerToken is StandardZipperToken
 {
     address issuer;
-    
-    function SimpleIssuerToken(address _issuer, ITokenActionValidator _validator, address _authedForwarder) 
+
+    function SimpleIssuerToken(address _issuer, ITokenActionValidator _validator, IAuthedForwarder _authedForwarder)
         StandardZipperToken(_validator, _authedForwarder)
     {
         issuer = _issuer;
     }
-    
+
     function issue(uint _amount) returns (bool success)
     {
         if (msg.sender != issuer)
@@ -253,7 +263,7 @@ contract SimpleIssuerToken is StandardZipperToken
         Transfer(this, msg.sender, _amount);
         return true;
     }
-     
+
     function redeem(uint _amount) returns (bool success)
     {
         if (msg.sender != issuer)
@@ -263,7 +273,7 @@ contract SimpleIssuerToken is StandardZipperToken
         Transfer(msg.sender, this, _amount);
         return true;
     }
-    
+
     function setIssuer(address _issuer) returns (bool success)
     {
         if (msg.sender != issuer)
@@ -275,16 +285,16 @@ contract SimpleIssuerToken is StandardZipperToken
 
 /**
  * The provisional ZIP token
-*/ 
+*/
 
 contract PZipToken is SimpleIssuerToken
 {
-    function PZipToken(address _issuer, ITokenActionValidator _validator, address _authedForwarder) 
+    function PZipToken(address _issuer, ITokenActionValidator _validator, IAuthedForwarder _authedForwarder)
          SimpleIssuerToken(_issuer, _validator, _authedForwarder)
     {
     }
 
    function name() constant returns (string result) { return "PZipToken"; }
    function symbol() constant returns (string result) { return "PZIP"; }
-   function decimals() constant returns (uint result) { return 10; }
+   function decimals() constant returns (uint result) { return 5; }
 }
